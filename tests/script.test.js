66fa5bb926850a86775094a8efbdedccd88674e1
anchi7777 <p24607036@student.nicmar.ac.in>
2026-03-22 23:31:47 +0530

    Add unit tests for WanderAI script functions

---
+++ tests/script.test.js
@@ -0,0 +1,7 @@
+const { functionName } = require('../path/to/wanderai/script'); // Adjust the path accordingly
+
+test('functionName does something', () => {
+    expect(functionName(args)).toBe(expectedResult);
+});
+
+// Add more tests as necessary
